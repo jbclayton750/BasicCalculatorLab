c166f26bcf2156efa4729b0ecb6ee75bbc3d2aa3
joseph.clayton750 <59981818+jbclayton750@users.noreply.github.com>
2020-01-23 16:52:17 -0600

    Update Program.cs

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -8,17 +8,20 @@ namespace BasicCalculator
         {
             Console.WriteLine("*** BASIC CALCULATOR ***");
 
-            Console.WriteLine("Enter the first number");
-
-            // int.Parse will take a string data type and convert it to an int data type
+            Console.WriteLine("Enter the temperature in Fahrenheit");
             int firstNumber = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second number");
+            Console.WriteLine("Enter the relative humidity");
             int secondNumber = int.Parse(Console.ReadLine());
+                        
+            int sum = firstNumber - ((9*(100-secondNumber))/25);
 
-            int sum = firstNumber + secondNumber;
+            Console.WriteLine("The dew point is " + sum);
 
-            Console.WriteLine("The answer is " + sum);
+            Console.WriteLine("Enter the wind speed");
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int windchill = 35.74 + (0.6125*firstNumber);
         }
     }
 }
